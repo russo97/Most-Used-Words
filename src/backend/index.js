5da377b5d43bf310fe0605775fcb810a029cbdc1
Wanderson Silva <wandersonsilva768@gmail.com>
2020-09-29 02:45:03 -0300

    feat: exibindo arquivos capturados mapeados em String no console do Electron

--- src/backend/index.js
+++ src/backend/index.js
@@ -1,15 +1,19 @@
 const { ipcMain } = require('electron');
 
-ipcMain.on('process-subtitles', (event, paths) => {
-    console.log(paths);
+const pathsToRows = require('./pathToRows');
 
-    event.reply('process-subtitles', [
-        { name: 'i', amount: 1234 },
-        { name: 'you', amount: 900 },
-        { name: 'he', amount: 853 },
-        { name: 'among', amount: 130 },
-        { name: 'us', amount: 174 },
-    ]);
+ipcMain.on('process-subtitles', (event, paths) => {
+    pathsToRows(paths)
+        .then(row => console.log(row))
+        .then(() => {
+            event.reply('process-subtitles', [
+                { name: 'i', amount: 1234 },
+                { name: 'you', amount: 900 },
+                { name: 'he', amount: 853 },
+                { name: 'among', amount: 130 },
+                { name: 'us', amount: 174 },
+            ]);
+        });
 });
 
 console.log('ok')
